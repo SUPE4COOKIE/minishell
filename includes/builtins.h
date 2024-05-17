15f1fc5ff8a114377464f0fd175c06358d521a25
sonam <sonam.crumiere@icloud.com>
2024-05-17 15:39:36 +0200

    Added env and echo prototype

--- includes/builtins.h
+++ includes/builtins.h
@@ -6,7 +6,7 @@
 /*   By: scrumier <scrumier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/17 13:30:33 by scrumier          #+#    #+#             */
-/*   Updated: 2024/05/17 13:32:10 by scrumier         ###   ########.fr       */
+/*   Updated: 2024/05/17 15:03:49 by scrumier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,7 @@
 # define BUILTINS_H
 
 int	builtin_cd(t_minishell *mshell, char **args);
+int	builtin_echo(t_minishell *mshell, char **args);
+int	builtin_env(t_minishell *mshell, char **args);
 
 #endif
