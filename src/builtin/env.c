b119c79aff51e255d1fb2af4f1a079a8b7e1175d
sonam <sonam.crumiere@icloud.com>
2024-05-17 15:00:45 +0200

    Added the correct return value

--- src/builtin/env.c
+++ src/builtin/env.c
@@ -6,13 +6,11 @@
 /*   By: scrumier <scrumier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/17 14:23:07 by scrumier          #+#    #+#             */
-/*   Updated: 2024/05/17 14:39:07 by scrumier         ###   ########.fr       */
+/*   Updated: 2024/05/17 14:58:48 by scrumier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
-
-// A mon avis il marche pas (enfin ca me parait trop court pour etre correct)
 
 int	builtin_env(t_minishell *mshell, char **args)
 {
@@ -20,12 +18,12 @@ int	builtin_env(t_minishell *mshell, char **args)
 
 	if (args && args[1])
 	{
-		ft_putstr_fd("env: too many arguments\n", STDERR_FILENO);
-		return (EXIT_FAILURE);
+		ft_putstr_fd("minishell: env: too many arguments\n", STDERR_FILENO);
+		return (127);
 	}
 	i = 0;
 	if (!mshell->env)
-		return (EXIT_FAILURE);
+		return (EXIT_SUCCESS);
 	while (mshell->env[i])
 	{
 		ft_putstr_fd(mshell->env[i], STDOUT_FILENO);
