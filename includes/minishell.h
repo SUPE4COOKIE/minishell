47cfed941df80c862c1ba128064ff9a79104b12b
SUPE4COOKIE <supe4cookie@gmail.com>
2024-06-05 19:46:14 +0200

    save

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,7 +6,7 @@
 /*   By: mwojtasi <mwojtasi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/24 17:18:24 by mwojtasi          #+#    #+#             */
-/*   Updated: 2024/06/05 15:21:03 by mwojtasi         ###   ########.fr       */
+/*   Updated: 2024/06/05 19:40:29 by mwojtasi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,7 +29,6 @@
 # ifndef PATH_MAX
 #  define PATH_MAX 4096
 # endif
-# define PRINT_CAT false
 
 typedef struct s_cmd	t_cmd;
 typedef struct s_lexer	t_lexer;
@@ -44,7 +43,6 @@ typedef enum e_token_type
 	T_HERE_DOC,
 	T_D_QUOTED_WORD,
 	T_S_QUOTED_WORD,
-	T_PATH,
 }				t_token_type;
 
 typedef struct s_minishell
