fedfcba7995c329f3e2155a663fddc335a181b7e
sonam <sonam.crumiere@icloud.com>
2024-06-18 15:13:03 +0200

    Removed set_sysbin

--- src/parsing/env_parse.c
+++ src/parsing/env_parse.c
@@ -50,41 +50,20 @@ static int	parse_path(t_minishell *mshell, char *env)
 	return (0);
 }
 
-static int	set_sysbin(t_minishell *mshell)
-{
-	mshell->path = malloc(sizeof(char *) * 2);
-	if (!mshell->path)
-		return (1); // TODO: add a free struct
-	mshell->path[0] = ft_strdup("/bin");
-	if (!mshell->path[0])
-		return (1); // TODO: add a free struct
-	mshell->path[1] = NULL;
-	return (0);
-}
-
 int	save_path(t_minishell *mshell, char **env)
 {
 	size_t	i;
 
 	i = 0;
-	if (!env || !env[0])
-		set_sysbin(mshell);
-	else
+	mshell->path = NULL;
+	while (env[i])
 	{
-		while (env[i])
+		if (ft_strncmp(env[i], "PATH=", 5) == 0)
 		{
-			if (ft_strncmp(env[i], "PATH=", 5) == 0)
-			{
-				if (env[i][5] == 0)
-					set_sysbin(mshell);
-				else
-					parse_path(mshell, env[i]);
-				return (0);
-			}
-			i++;
+			parse_path(mshell, env[i]);
+			return (0);
 		}
+		i++;
 	}
-	if (!mshell->path)
-		set_sysbin(mshell);
 	return (0);
 }
