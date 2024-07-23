ae165e605f70c9dffc4e7dc1fdd67294215b5a7c
sonam <sonam.crumiere@icloud.com>
2024-07-23 14:07:49 +0200

    norm patch

--- src/exec/fork_exec.c
+++ src/exec/fork_exec.c
@@ -6,7 +6,7 @@
 /*   By: scrumier <scrumier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/14 14:19:44 by scrumier          #+#    #+#             */
-/*   Updated: 2024/07/22 19:43:59 by scrumier         ###   ########.fr       */
+/*   Updated: 2024/07/23 14:06:57 by scrumier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -53,7 +53,7 @@ void	fork_exec(t_minishell *mshell, int old[2], int new[2], int i)
 	cmd = init_before_fork(&y, mshell, &id, i);
 	if (is_builtin(cmd->cmd) == false || \
 			(is_builtin(cmd->cmd) == true && mshell->cmds->next != NULL))
-			id = fork();
+		id = fork();
 	if (id != 0)
 		mshell->last_pid = id;
 	if (id == -1)
